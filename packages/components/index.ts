96451259851eaf9ef2398e13dbf74189245b08c0
rookie-hhm <3176152005@qq.com>
2022-08-24 18:10:39 +0800

    dd

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -1,24 +1,20 @@
 import type { App } from 'vue'
-// const modules = import.meta.globEager('./**/index.ts')
-// const componentList: any[] = []
+const modules = import.meta.globEager('./**/index.ts')
+const componentList: any[] = []
 
-// const getComponentName = (path: string): string => {
-//   return path.replace(/\.\/(.*)\/index\.ts$/, '$1')
-// }
+const getComponentName = (path: string): string => {
+  return path.replace(/\.\/(.*)\/index\.ts$/, '$1')
+}
 
-// const registerGlobalComponent = (app: App) => {
-//   for (const key in modules) {
-//     const module = modules[key]
-//     const name = getComponentName(key)
-//     componentList.push({ name, default: module.default })
-//   }
-//   componentList.forEach(component => {
-//     app.component(`R${component.name}`, component.default)
-//   })
-// }
-import Table from './Table'
 const registerGlobalComponent = (app: App) => {
-  app.component('r-table', Table)
+  for (const key in modules) {
+    const module = modules[key]
+    const name = getComponentName(key)
+    componentList.push({ name, default: module.default })
+  }
+  componentList.forEach(component => {
+    app.component(`R${component.name}`, component.default)
+  })
 }
 import '../styles/index.scss'
 
